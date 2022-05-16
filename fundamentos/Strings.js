b5f88328546cc30080f1bf352b3d6ce56ba34d24
Gakkou0 <fhugo.625@gmail.com>
2022-05-16 09:53:41 -0300

    Adição dos estudos de  Math e String

---
+++ fundamentos/Strings.js
@@ -0,0 +1,13 @@
+const escola = "Cartola"
+
+console.log(escola.charAt(2))
+console.log(escola.charCodeAt(4))
+console.log(escola.indexOf("l"))
+
+console.log(escola.substring(1))
+console.log(escola.substring(0, 6))
+
+console.log("Escola " + escola  + " uau")
+console.log(escola.replace(/\w/g, "o"))
+
+console.log("Ana,Maria,Pedro,João".split(","))
