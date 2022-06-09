f15369681bba8e9bce084d682e3f76d5edfb4f99
Gakkou0 <fhugo.625@gmail.com>
2022-06-09 16:00:59 -0300

    adição das questões de 1 a 4 da primeira lista

---
+++ exercicios/questao01.js
@@ -0,0 +1,11 @@
+/*Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, subtração,
+multiplicação e divisão desses valores.*/
+
+function operacoes (num1, num2){
+    console.log(num1 + num2)
+    console.log(num1 - num2)
+    console.log(num1 * num2)
+    console.log(num1 / num2)
+}
+
+operacoes(2, 5)
---
+++ exercicios/questao03.js
@@ -0,0 +1,7 @@
+/*Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.*/
+
+const elevado = (base, expoente) => console.log(Math.pow(base, expoente))
+
+for (let i = 0; i<= 10; i++){
+    elevado(2, i)
+}
