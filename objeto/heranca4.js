13028e36bf2bca74a418035a125941414b3388ff
Gakkou0 <fhugo.625@gmail.com>
2022-10-16 13:57:36 -0300

    adição de herança 3 e 4

---
+++ objeto/heranca4.js
@@ -0,0 +1,15 @@
+function MeuObjeto() {}
+console.log(MeuObjeto.prototype)
+
+const obj1 = new MeuObjeto
+const obj2 = new MeuObjeto
+
+console.log(obj1.__proto__ === obj2.__proto__)
+console.log(MeuObjeto.prototype === obj2.__proto__)
+
+MeuObjeto.prototype.nome = 'Antonio'
+MeuObjeto.prototype.falar = function () {
+    console.log(`Bom dia: meu nome é ${this.nome}!`)
+}
+
+obj1.falar()
